8e3c634ba95a5a502e122aa0d2801f4d8cd0573d
Alvee <M.Alvee8141@gmail.com>
2024-05-30 19:40:45 +0600

    changes and updates

--- stax/tools/chocolateyinstall.ps1
+++ stax/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop' # stop on all errors
+﻿# $ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # Function to determine architecture and select Dart SDK URL accordingly
@@ -15,34 +15,28 @@ function Get-DartSdkUrl {
 # Function to download and install Dart SDK
 function Install-DartSDK {
     $dartSdkUrl = Get-DartSdkUrl
-    $dartTempDir = "$env:TEMP\DartSDK"
     $dartSdkPath = "$toolsDir\Dart"
+    # Dart\dart-sdk\bin
 
     # Create temporary directory for Dart SDK
-    if (!(Test-Path $dartTempDir)) {
-        New-Item -ItemType Directory -Path $dartTempDir | Out-Null
+    if (!(Test-Path $dartSdkPath)) {
+        New-Item -ItemType Directory -Path $dartSdkPath | Out-Null
     }
 
     # Download Dart SDK
     Write-Host "Downloading Dart SDK..."
-    $dartZipFile = "$dartTempDir\dartsdk.zip"
+    $dartZipFile = "$dartSdkPath\dartsdk.zip"
     Invoke-WebRequest -Uri $dartSdkUrl -OutFile $dartZipFile
 
     # Extract Dart SDK
     Write-Host "Extracting Dart SDK..."
-    Expand-Archive -Path $dartZipFile -DestinationPath $dartTempDir
-
-    # Move Dart SDK to tools directory
-    Move-Item -Path "$dartTempDir\dart-sdk" -Destination $dartSdkPath -Force
-
-    # Cleanup
-    Remove-Item $dartTempDir -Recurse -Force
+    Expand-Archive -Path $dartZipFile -DestinationPath $dartSdkPath
 }
 
 # Function to clone and build Dart CLI App
 function Build-DartApp {
     $appRepoUrl = "https://github.com/TarasMazepa/stax.git"
-    $appFolder = "$toolsDir\dart_app"
+    $appFolder = "$toolsDir\staxcli"
 
     # Clone App Repository
     Write-Host "Cloning Dart CLI App repository..."
@@ -53,8 +47,19 @@ function Build-DartApp {
 
     # Build Dart App
     Write-Host "Building Dart CLI App..."
-    & "$toolsDir\Dart\bin\dart" pub get
-    & "$toolsDir\Dart\bin\dart" compile exe bin/cli.dart -o stax
+    & "$toolsDir\Dart\dart-sdk\bin\dart" pub get
+    & "$toolsDir\Dart\dart-sdk\bin\dart" compile exe bin/cli.dart -o stax
+
+    # Move binary to user folder
+    $userBinFolder = [Environment]::GetFolderPath("UserProfile") + "\bin"
+    if (!(Test-Path $userBinFolder)) {
+        New-Item -ItemType Directory -Path $userBinFolder | Out-Null
+    }
+    Move-Item -Path "$toolsDir\staxcli\cli\stax" -Destination $userBinFolder -Force
+
+    # Add binary to PATH
+    $env:Path += ";$userBinFolder"
+    [Environment]::SetEnvironmentVariable("Path", $env:Path, [EnvironmentVariableTarget]::User)
 
     # Cleanup
     Remove-Item $appFolder -Recurse -Force
