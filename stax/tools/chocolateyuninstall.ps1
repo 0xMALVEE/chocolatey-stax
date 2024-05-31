c41823d4e004a73e0ae4e9f65a000e3680c13ea3
Alvee <M.Alvee8141@gmail.com>
2024-05-31 20:13:50 +0600

    remove bin folder from path on uninstall script

--- stax/tools/chocolateyuninstall.ps1
+++ stax/tools/chocolateyuninstall.ps1
@@ -9,6 +9,9 @@ function Uninstall-DartApp {
     # Remove Dart CLI binary and its parent folder
     Remove-Item -Path "$staxBinaryFolder\stax" -Force
     Remove-Item -Path $staxBinaryFolder -Force
+
+    # Remove the directory from the user-level PATH
+    [Environment]::SetEnvironmentVariable("Path", $env:Path -replace [regex]::Escape("$staxBinaryFolder;"), "User")
 }
 
 # Main Script
